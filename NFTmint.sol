b9e0496f586b653008aef66cd115a7ebe68a0ad8
mhsendur <120842010+mhsendur@users.noreply.github.com>
2023-02-08 10:08:52 +0300

    Lazymint development

--- NFTmint.sol
+++ NFTmint.sol
@@ -1,8 +1,36 @@
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
-contract NFTLazyMint{
+import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+
+contract NFTLazyMint is EIP712{
 
     IERC721 NFT ; 
     address mpadress ; 
+    mapping(uint => bool) private tokens;
+    address owner;
+
+    struct NFTVoucher {
+    uint256 tokenId;
+    uint256 minPrice; //To allow the creator to set a price when redeem function is used, value is greater than zero.
+    string uri;
+    bytes signature;
+    }
+
+    function createVoucher(tokenId, uri, minPrice){
+        minPrice = 0;
+        voucher = {tokenId, uri, minPrice}
+
+    }
+
+    const lazyminter = new LazyMinter({ myDeployedContract.address, signerForMinterAccount })
+
+
+    function mint(uint tokenId) public {
+        require(!tokens[tokenId], "Token already exists");
+
+        //Mint new token
+        tokens[tokenId] = true;
+    }
+
 
 }
