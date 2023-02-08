fd8bac02579d61b134def6a5850bfd379b1cc3fd
mertcoskuner <mertcoskuner@gmail.com>
2023-02-08 11:10:43 +0300

    mint contract updated

--- NFTmint.sol
+++ NFTmint.sol
@@ -1,9 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract NFTLazyMint{
+pragma solidity ^0.8.7;
 
-    IERC721 NFT ; 
-    address mpadress ; 
+contract LazyMinting {
+    uint256 public totalSupply;
 
+    mapping (address => uint256) public balances;
+    mapping (address => mapping (uint256 => bool)) public minted;
+
+    event NewMint(address indexed _to, uint256 _tokenId);
+
+    function mint(address _to, uint256 _tokenId) public {
+        require(!minted[_to][_tokenId], "Token already minted");
+        minted[_to][_tokenId] = true;
+        balances[_to]++;
+        totalSupply++;
+        emit NewMint(_to, _tokenId);
+    }
+
+    function balanceOf(address _owner) public view returns (uint256) {
+        return balances[_owner];
+    }
 }
